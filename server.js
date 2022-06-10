b541d01f44a1f810a344fc826f646cfe0a70a243
lyf703331869 <yliu52@nyit.edu>
2022-06-10 00:25:43 -0400

    Add POST request function

--- server.js
+++ server.js
@@ -4,16 +4,35 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+// POST notes to database
+app.post("/api/notes", (req, res) => {
+  let newNote = req.body;
+
+  fs.readFile("/db/db.json", (err, notes) => {
+    if (err) throw err;
+    const notesArr = JSON.parse(notes);
+    notesArr.push(newNote);
+    // write files
+    fs.writeFile("/db/db.json", JSON.stringify(notesArr)),
+      "utf8",
+      (err) => {
+        if (err) console.log(err);
+        res.json(newNote);
+      };
+  });
+});
 
 // GET Route for homepage
 app.get("/", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/index.html"))
 );
 
-// GET Route for nodes page
+// GET Route for notes page
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
