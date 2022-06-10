97321a75fc8b1f998467357335947bf73ab302c1
lyf703331869 <yliu52@nyit.edu>
2022-06-10 00:01:42 -0400

    Add GET Route for homepage and notes page

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
+const fs = require("fs");
 const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.json(`${req.method} request received`);
-});
+// GET Route for homepage
+app.get("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
+// GET Route for nodes page
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
+);
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
