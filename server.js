a27b5010d50805dc2ae7be7827f3698a886202f2
lyf703331869 <yliu52@nyit.edu>
2022-06-10 19:59:28 -0400

    Add delete request

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const fs = require("fs");
 const express = require("express");
 const notes = require("./db/db.json");
 const path = require("path");
+const uniqId = require("uniqid");
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
@@ -13,18 +15,23 @@ app.use(express.static("public"));
 // POST to create note to notes database
 app.post("/api/notes", (req, res) => {
   let newNote = req.body;
+  let noteId = uniqId();
+  newNote.id = noteId;
 
-  fs.readFile("/db/db.json", (err, notes) => {
+  fs.readFile("./db/db.json", (err, notes) => {
     if (err) throw err;
     const notesArr = JSON.parse(notes);
     notesArr.push(newNote);
     // write files
-    fs.writeFile("./db/db.json", JSON.stringify(notesArr, null, 2)),
+    fs.writeFile(
+      "./db/db.json",
+      JSON.stringify(notesArr, null, 2),
       "utf8",
       (err) => {
         if (err) return console.log(err);
         res.json(newNote);
-      };
+      }
+    );
   });
 });
 
@@ -41,6 +48,33 @@ app.get("/notes", (req, res) =>
 // GET to read all current notes from database
 app.get("/api/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "./db/db.json"));
+});
+
+// DELETE
+app.delete("/api/notes/:id", (req, res) => {
+  const id = req.params.id;
+  console.log(id);
+  fs.readFile("./db/db.json", (err, notes) => {
+    if (err) throw err;
+    let notesArr = JSON.parse(notes);
+    console.log(notesArr[0].id);
+
+    for (let i = 0; i < notesArr.length; i++) {
+      if (id === notesArr[i].id) {
+        notesArr.splice(i, 1);
+      }
+    }
+
+    fs.writeFile(
+      "./db/db.json",
+      JSON.stringify(notesArr, null, 2),
+      "utf8",
+      (err) => {
+        if (err) return console.log(err);
+        res.json(`note with id: ${id} has been deleted`);
+      }
+    );
+  });
 });
 
 app.get("/api/notes/:id", (req, res) => {
